57910b601ca7d40c6c588ef32b9cfb4af491b778
Tara <81445969+Tara-Q@users.noreply.github.com>
2022-01-21 19:21:12 +0000

    Removed query

---
+++ server/scripts/create_tables.sql
@@ -0,0 +1,19 @@
+-- i was thinking of creating these tables below as a way to introduce some relational elements? 
+
+-- CREATE TABLE booking_status
+-- (
+--     booking_status_id SERIAL PRIMARY KEY,
+--     status VARCHAR
+-- );
+
+-- CREATE TABLE residency_status 
+-- ( 
+--     residency_status_id SERIAL PRIMARY KEY,
+--     status VARCHAR
+-- );
+
+-- CREATE TABLE gender
+-- (
+--     gender_id SERIAL PRIMARY KEY,
+--     gender  VARCHAR
+-- );
