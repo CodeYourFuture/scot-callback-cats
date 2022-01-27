2f5437024a4faacfa3e1f2844d93c85ecf5517db
Tara <81445969+Tara-Q@users.noreply.github.com>
2022-01-27 14:56:58 +0000

    changed modal body to a textarea

--- client/src/Modal.js
+++ client/src/Modal.js
@@ -1,10 +1,11 @@
+/* eslint-disable jsx-a11y/label-has-for */
 import { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 
 function SMSModal() {
     const [show, setShow] = useState(false);
-    const [disabled, setDisabled] = useState(true);
+    const [disabled, setDisabled] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
@@ -27,7 +28,12 @@ function SMSModal() {
           <Modal.Header closeButton>
             <Modal.Title>Send SMS</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Hi, this is Bikes For Refugees. Bikes are now available for you to pick up. Please click LINK to choose a pick up date. For any questions, please contact 07900000000.</Modal.Body>
+          <Modal.Body>
+            <form>
+              <label className="form-text text-muted" htmlFor="enterMessageText">Enter your message below</label>
+              <textarea className="form-control border-radius" id='text' name='text' defaultValue="Hi, this is Bikes For Refugees. Bikes are now available for you to pick up. Please click LINK to choose a pick up date. For any questions, please contact 07900000000." />
+            </form>
+          </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
               Cancel
