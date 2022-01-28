2dab8f28470f43e9a8f56cecdc31758c232f3d6c
aalassv <a.alas.sv@gmail.com>
2022-01-28 21:57:32 +0000

    Adding validation to the clients post route

--- server/api.js
+++ server/api.js
@@ -11,91 +11,63 @@ router.get("/clients", (req, res) => {
 	db.query("SELECT * from clients;")
 		.then((result) => res.json(result.rows))
 		.catch((e) => {
-			console.error(e);
-			res.sendStatus(400);
+			res.status(400).send(e);
 		});
 });
 
-router.post("/clients", (req, res) => {
-	if (Array.isArray(req.body)) {
- return req.body.forEach((element) => {
-			const newClientDateAdded = element.date_added;
-			const newClientDateAddedIso = new Date(
-				Date.parse(newClientDateAdded)
-			).toISOString();
-			const newClientName = element.name;
-			const newClientBikes = element.bikes_needed;
-			const newClientPhoneNumber = element.phone_number;
-			const newClientBookingStatus = element.booking_status;
-			const newClientResidencyStatus = element.residency_status;
-			const newClientCountryOfOrigin = element.country_of_origin;
-			const newClientTimeInScotland = element.time_in_scotland;
-			const newClientLanguageSpoken = element.language_spoken;
-			const newClientEnglishSpeaker = element.english_speaker;
-			const newClientEnglishSkillLevel = element.english_skill_level;
-			const newClientGender = element.gender;
-			const newClientDateOfBirth = element.date_of_birth;
-			const newClientDateOfBirthIso = new Date(
-				Date.parse(newClientDateOfBirth)
-			).toISOString();
-			const newClientPostcode = element.postcode;
-			const newClientReferringAgency = element.referring_agency;
-
-			const createQuery =
-				"INSERT INTO clients (date_added, name,  bikes_needed, phone_number, booking_status,residency_status, country_of_origin, time_in_scotland, language_spoken, english_speaker, english_skill_level, gender, date_of_birth, postcode, referring_agency) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)";
-			db.query(createQuery, [
-				newClientDateAddedIso,
-				newClientName,
-				newClientBikes,
-				newClientPhoneNumber,
-				newClientBookingStatus,
-				newClientResidencyStatus,
-				newClientCountryOfOrigin,
-				newClientTimeInScotland,
-				newClientLanguageSpoken,
-				newClientEnglishSpeaker,
-				newClientEnglishSkillLevel,
-				newClientGender,
-				newClientDateOfBirthIso,
-				newClientPostcode,
-				newClientReferringAgency,
-			])
-				.then(() => res.json(req.body))
-				.catch((e) => {
-					console.error(e);
-					res.sendStatus(400);
-				});
-		});
-	} else {
-		const newClientDateAdded = req.body.date_added;
+const saveUser = async (element) => {
+	return new Promise((resolve, reject) => {
+		const newClientDateAdded = element.date_added;
+		if (!newClientDateAdded || isNaN(!newClientDateAdded)) {
+			return reject(
+				"The field date added is empty or is not a date, it is require to have a date"
+			);
+		}
 		const newClientDateAddedIso = new Date(
 			Date.parse(newClientDateAdded)
 		).toISOString();
-		const newClientName = req.body.name;
-		const newClientBikes = req.body.bikes_needed;
-		const newClientPhoneNumber = req.body.phone_number;
-		const newClientBookingStatus = req.body.booking_status;
-		const newClientResidencyStatus = req.body.residency_status;
-		const newClientCountryOfOrigin = req.body.country_of_origin;
-		const newClientTimeInScotland = req.body.time_in_scotland;
-		const newClientLanguageSpoken = req.body.language_spoken;
-		const newClientEnglishSpeaker = req.body.english_speaker;
-		const newClientEnglishSkillLevel = req.body.english_skill_level;
-		const newClientGender = req.body.gender;
-		const newClientDateOfBirth = req.body.date_of_birth;
+		const newClientName = element.name;
+		if (!newClientName) {
+			return reject(
+				"The field name is empty and it is require to have client name"
+			);
+		}
+		const newClientBikes = element.bikes_needed;
+		if (!newClientBikes || isNaN(!newClientBikes)) {
+			return reject(
+				"The field bikes is empty or is not a number, it is require to have a numerical value"
+			);
+		}
+		const newClientPhoneNumber = element.phone_number;
+		if (!newClientPhoneNumber || isNaN(!newClientPhoneNumber)) {
+			return reject(
+				"The field phone number is empty or is not a number, it is require to have a numerical value"
+			);
+		}
+		// 	db.query("SELECT phone_number FROM clients WHERE phone_number = $1",[newClientPhoneNumber]) === newClientPhoneNumber );
+		// }
+		const newClientBookingStatus = element.booking_status;
+		const newClientResidencyStatus = element.residency_status;
+		const newClientCountryOfOrigin = element.country_of_origin;
+		const newClientTimeInScotland = element.time_in_scotland;
+		const newClientLanguageSpoken = element.language_spoken;
+		const newClientEnglishSpeaker = element.english_speaker;
+		const newClientEnglishSkillLevel = element.english_skill_level;
+		const newClientGender = element.gender;
+		const newClientDateOfBirth = element.date_of_birth;
 		const newClientDateOfBirthIso = new Date(
 			Date.parse(newClientDateOfBirth)
 		).toISOString();
-		const newClientPostcode = req.body.postcode;
-		const newClientReferringAgency = req.body.referring_agency;
+		const newClientPostcode = element.postcode;
+		const newClientReferringAgency = element.referring_agency;
 
 		const createQuery =
-			"INSERT INTO clients (date_added, name, phone_number, bikes_needed, booking_status,residency_status, country_of_origin, time_in_scotland, language_spoken, english_speaker, english_skill_level, gender, date_of_birth, postcode, referring_agency) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)";
+			"INSERT INTO clients (date_added, name,  bikes_needed, phone_number, booking_status,residency_status, country_of_origin, time_in_scotland, language_spoken, english_speaker, english_skill_level, gender, date_of_birth, postcode, referring_agency) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)";
 		db.query(createQuery, [
 			newClientDateAddedIso,
 			newClientName,
-			newClientPhoneNumber,
 			newClientBikes,
+			newClientPhoneNumber,
 			newClientBookingStatus,
 			newClientResidencyStatus,
 			newClientCountryOfOrigin,
@@ -108,11 +80,27 @@ router.post("/clients", (req, res) => {
 			newClientPostcode,
 			newClientReferringAgency,
 		])
-			.then(() => res.json(req.body))
+			.then(() => resolve())
 			.catch((e) => {
-				console.error(e);
-				res.sendStatus(400);
+				reject(e);
 			});
+	});
+};
+
+router.post("/clients", (req, res) => {
+	if (Array.isArray(req.body)) {
+		const promises = [];
+		req.body.forEach((element) => {
+			const promiseResult = saveUser(element);
+			promises.push(promiseResult);
+		});
+		Promise.all(promises)
+			.then(() => res.status(200).send("All clients were inserted"))
+			.catch((e) => res.status(500).send(e));
+	} else {
+		saveUser(req.body)
+			.then(() => res.status(200).send("One clients was inserted"))
+			.catch((e) => res.status(500).send(e));
 	}
 });
 
