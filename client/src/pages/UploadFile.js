5a07a93ad8effc416b5e9b537e3e10dde3807f5c
kenda <mariayousef2017@gmail.com>
2022-01-31 14:27:32 +0000

    css for page

--- client/src/pages/UploadFile.js
+++ client/src/pages/UploadFile.js
@@ -1,37 +1,37 @@
-
-
+import icon from "./icon.svg";
+import "./UploadFile.css";
 import CSVReader from "react-csv-reader";
-const UploadFile = () =>  {
+const UploadFile = () => {
 
-const handleupload = (data, fileInfo, originalFile) =>{
-	 const response =  fetch("/api/upload", {
+	const handleupload = (data, fileInfo, originalFile) => {
+		const response = fetch("/api/upload", {
 			method: "POST",
 
 			headers: {
 				"Content-Type": "application/json",
-
 			},
 
 			body: JSON.stringify(data),
 		});
-};
+	};
 	return (
 		<form>
 			<div>
 				<h4>Upload Page</h4>
 			</div>
-			<div className="container">
+			<div className="container p-5">
 				<div className="row">
 					<div className="col align-self-center">
 						<div className="input-group">
 							<CSVReader onFileLoaded={handleupload} accept=".csv" />
-
-							<button
-								className="btn btn-outline-secondary"
-								type="button"
-								id="inputGroupFileAddon04"
-							>
-								Submit
+							<button type="button" className="btn btn-secondary  ">
+								<img
+									className="icon"
+									data-qa="icon"
+									src={icon}
+									alt="upload icon"
+								/>{" "}
+								UploadFile
 							</button>
 						</div>
 					</div>
