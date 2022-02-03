b7a7e5331124e3bc96c568e7a6d18415e2f179c7
Tara <81445969+Tara-Q@users.noreply.github.com>
2022-02-03 13:11:59 +0000

    Renamed functions and removed some redundant state

--- client/src/Component/SMSModal.js
+++ client/src/Component/SMSModal.js
@@ -5,38 +5,37 @@ import Button from "react-bootstrap/Button";
 
 function SMSModal(props) {
     const [show, setShow] = useState(false);
-    const [disabled, setDisabled] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const hideModal = () => setShow(false);
+    const showModal = () => setShow(true);
 
 
     function handleSubmit () {
       console.log(props.checkedCheckboxes);
-      handleClose();
+      hideModal();
       }
 
 
     return (
       <>
-        <Button variant="primary" onClick={handleShow} disabled={props.checkedCheckboxes.length === 0}>
+        <Button variant="primary" onClick={showModal} disabled={props.checkedCheckboxes.length === 0}>
           Send SMS
         </Button>
 
-        <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal show={show} onHide={hideModal} animation={false}>
           <Modal.Header closeButton>
             <Modal.Title>Send SMS</Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <form>
-              <label className="form-text text-muted" htmlFor="enterMessageText">Enter your message below</label>
+              <label className="form-text text-muted" htmlFor="text">Enter your message</label>
               <textarea className="form-control border-radius" id='text' name='text' defaultValue="Hi, this is Bikes For Refugees. Bikes are now available for you to pick up. Please click LINK to choose a pick up date. For any questions, please contact 07900000000." />
             </form>
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
+            <Button variant="secondary" onClick={hideModal}>
               Cancel
             </Button>
-            <Button variant="primary" type = "submit" onClick={handleSubmit}>
+            <Button variant="primary" onClick={handleSubmit}>
               Send SMS
             </Button>
           </Modal.Footer>
