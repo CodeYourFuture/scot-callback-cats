1ce7af9ca6278589b67c38331a45d742848e4857
aalassv <a.alas.sv@gmail.com>
2022-02-04 17:54:10 +0000

    Updating post request and adding two new columns

--- server/api.js
+++ server/api.js
@@ -32,23 +32,35 @@ const saveUser = (client) => {
 		dateOfBirth,
 		postcode,
 		referringAgency,
+		pickUpDate,
+		isDeclined,
 	} = client;
 
 	const validationErrors = [];
 
 	if (!dateAdded) {
-		validationErrors.push("The field dateAdded is empty. It is required to have a date");
+		validationErrors.push(
+			"The field dateAdded is empty. It is required to have a date"
+		);
 	}
 	if (!name) {
-		validationErrors.push("The field name is empty. It is requires to have a name");
+		validationErrors.push(
+			"The field name is empty. It is requires to have a name"
+		);
 	}
 	if (!bikesNeeded || isNaN(Number(bikesNeeded))) {
-		validationErrors.push("The field bikesNeeded is empty or it is not a number. It is required to have a numerical value");
+		validationErrors.push(
+			"The field bikesNeeded is empty or it is not a number. It is required to have a numerical value"
+		);
 	}
 	if (!phoneNumber) {
-		validationErrors.push("The field phoneNumber is empty. It is required to have a value");
+		validationErrors.push(
+			"The field phoneNumber is empty. It is required to have a value"
+		);
 	}
-
+	if (isDeclined === undefined) {
+		validationErrors.push("The field isDeclined is empty");
+	}
 	if (validationErrors.length) {
 		return Promise.reject(validationErrors);
 	}
@@ -56,11 +68,11 @@ const saveUser = (client) => {
 	const dateAddedIso = new Date(dateAdded).toISOString();
 	const dateOfBirthIso = new Date(dateOfBirth).toISOString();
 	const createQuery =
-		"INSERT INTO clients (date_added, name,  bikes_needed, phone_number, booking_status,residency_status, country_of_origin, time_in_scotland, language_spoken, english_speaker, english_skill_level, gender, date_of_birth, postcode, referring_agency)" +
-		"VALUES" +
-		"($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)" +
-		"ON CONFLICT (phone_number)" +
-		"DO UPDATE SET date_added = EXCLUDED.date_added, name = EXCLUDED.name, bikes_needed = EXCLUDED.bikes_needed, booking_status = EXCLUDED.booking_status, residency_status = EXCLUDED.residency_status, country_of_origin = EXCLUDED.country_of_origin, time_in_scotland = EXCLUDED.time_in_scotland, language_spoken = EXCLUDED.language_spoken, english_speaker = EXCLUDED.english_speaker, english_skill_level = EXCLUDED.english_skill_level, gender = EXCLUDED.gender, date_of_birth = EXCLUDED.date_of_birth, postcode = EXCLUDED.postcode, referring_agency = EXCLUDED.referring_agency";
+		"INSERT INTO clients (date_added, name,  bikes_needed, phone_number, booking_status,residency_status, country_of_origin, time_in_scotland, language_spoken, english_speaker, english_skill_level, gender, date_of_birth, postcode, referring_agency, pick_up_date, is_declined)" +
+		" VALUES " +
+		" ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)" +
+		" ON CONFLICT (phone_number)" +
+		" DO UPDATE SET date_added = EXCLUDED.date_added, name = EXCLUDED.name, bikes_needed = EXCLUDED.bikes_needed, booking_status = EXCLUDED.booking_status, residency_status = EXCLUDED.residency_status, country_of_origin = EXCLUDED.country_of_origin, time_in_scotland = EXCLUDED.time_in_scotland, language_spoken = EXCLUDED.language_spoken, english_speaker = EXCLUDED.english_speaker, english_skill_level = EXCLUDED.english_skill_level, gender = EXCLUDED.gender, date_of_birth = EXCLUDED.date_of_birth, postcode = EXCLUDED.postcode, referring_agency = EXCLUDED.referring_agency, pick_up_date = EXCLUDED.pick_up_date, is_declined = EXCLUDED.is_declined";
 
 	return db.query(createQuery, [
 		dateAddedIso,
@@ -78,6 +90,8 @@ const saveUser = (client) => {
 		dateOfBirthIso,
 		postcode,
 		referringAgency,
+		pickUpDate,
+		isDeclined,
 	]);
 };
 
@@ -116,36 +130,43 @@ const updateUser = (client, clientId) => {
 		pickUpDate,
 		isDeclined,
 	} = client;
-console.log(client);
+	console.log(client);
 	const validationErrors = [];
 
 	if (!dateAdded) {
-		validationErrors.push("The field dateAdded is empty. It is required to have a date");
+		validationErrors.push(
+			"The field dateAdded is empty. It is required to have a date"
+		);
 	}
 	if (!name) {
-		validationErrors.push("The field name is empty. It is requires to have a name");
+		validationErrors.push(
+			"The field name is empty. It is requires to have a name"
+		);
 	}
 	if (!bikesNeeded || isNaN(Number(bikesNeeded))) {
-		validationErrors.push("The field bikesNeeded is empty or it is not a number. It is required to have a numerical value");
+		validationErrors.push(
+			"The field bikesNeeded is empty or it is not a number. It is required to have a numerical value"
+		);
 	}
 	if (!phoneNumber) {
-		validationErrors.push("The field phoneNumber is empty. It is required to have a value");
+		validationErrors.push(
+			"The field phoneNumber is empty. It is required to have a value"
+		);
 	}
 
 	if (validationErrors.length) {
 		console.log("validation");
-		return Promise.reject (validationErrors);
-
+		return Promise.reject(validationErrors);
 	}
 
 	const createQuery =
-	"UPDATE clients " +
-	"SET " +
-	"date_added = $1, name = $2,  bikes_needed = $3, phone_number = $4, booking_status = $5,residency_status = $6, country_of_origin = $7, time_in_scotland = $8, language_spoken = $9, english_speaker = $10, english_skill_level = $11, gender = $12, date_of_birth = $13, postcode = $14, referring_agency = $15, pick_up_date = $16, is_declined = $17 " +
-	" WHERE client_id = $18";
+		"UPDATE clients " +
+		"SET " +
+		"date_added = $1, name = $2,  bikes_needed = $3, phone_number = $4, booking_status = $5,residency_status = $6, country_of_origin = $7, time_in_scotland = $8, language_spoken = $9, english_speaker = $10, english_skill_level = $11, gender = $12, date_of_birth = $13, postcode = $14, referring_agency = $15, pick_up_date = $16, is_declined = $17 " +
+		" WHERE client_id = $18";
 
-	return db.query(createQuery,
-		[dateAdded,
+	return db.query(createQuery, [
+		dateAdded,
 		name,
 		bikesNeeded,
 		phoneNumber,
@@ -169,8 +190,8 @@ console.log(client);
 router.put("/clients/:clientId", (req, res) => {
 	const { clientId } = req.params;
 	updateUser(req.body, clientId)
-	.then(() => res.status(200).send("Client updated"))
-	.catch((e) => res.status(500).send(e));
+		.then(() => res.status(200).send("Client updated"))
+		.catch((e) => res.status(500).send(e));
 });
 
 export default router;
