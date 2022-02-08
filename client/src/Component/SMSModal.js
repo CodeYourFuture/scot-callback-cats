c268724041ed4fd9ea2a323580a2bb5d1815f542
Tara <81445969+Tara-Q@users.noreply.github.com>
2022-02-08 21:48:11 +0000

    Add state  and onchange for message in modal

--- client/src/Component/SMSModal.js
+++ client/src/Component/SMSModal.js
@@ -2,15 +2,48 @@
 import { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import e from "express";
+
 
 function SMSModal(props) {
     const [show, setShow] = useState(false);
     const hideModal = () => setShow(false);
     const showModal = () => setShow(true);
+    let localMessage = localStorage.getItem("message");
+    if (localMessage === null) {
+      localMessage = "Hi, this is Bikes For Refugees. Bikes are now available for you to pick up. Please click LINK to choose a pick up date. For any questions, please contact 07900000000.";
+    }
+    const [message, setMessage] = useState(localMessage);
+
+    const handleChange = (e) => {
+      localStorage.setItem("message", e.target.value);
+      setMessage(e.target.value);
+    };
 
 
     function handleSubmit () {
+
+      // const newMessageRequest = {
+      //   ids : props.checkedCheckboxes,
+      //   // message: e.target.value,
+      // };
+
+      // fetch ("/api/send-messages", {
+      //   method: "POST",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify(newMessageRequest),
+      // })
+      // .then((response) => response.json());
+      //     // if(response.status) {
+
+      //     // } else {
+
+      //     // }
+
       console.log(props.checkedCheckboxes);
+      console.log(newMessageRequest);
       hideModal();
       }
 
@@ -28,7 +61,7 @@ function SMSModal(props) {
           <Modal.Body>
             <form>
               <label className="form-text text-muted" htmlFor="text">Enter your message</label>
-              <textarea className="form-control border-radius" id='text' name='text' defaultValue="Hi, this is Bikes For Refugees. Bikes are now available for you to pick up. Please click LINK to choose a pick up date. For any questions, please contact 07900000000." />
+              <textarea className="form-control border-radius" id='text' name='text' value={message} onChange={handleChange} />
             </form>
           </Modal.Body>
           <Modal.Footer>
