d7d53f46906ee00d0b67d3b6976e18f196526d33
aalassv <a.alas.sv@gmail.com>
2022-02-10 18:49:58 +0000

    Adding the Link requested in the review

--- client/src/Component/NavbarComponent.js
+++ client/src/Component/NavbarComponent.js
@@ -7,12 +7,12 @@ const NavbarComponent = () => {
 	return (
 		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
 		<Container>
-		<Navbar.Brand href="#home">CYF | SMS Project</Navbar.Brand>
+		<Navbar.Brand as={Link} to="/">CYF | Bike for Refuges SMS</Navbar.Brand>
 		<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 		<Navbar.Collapse id="responsive-navbar-nav">
 		  <Nav className="me-auto">
-			<Nav.Link href="/">Home</Nav.Link>
-			<Nav.Link href="/calendar">Calendar</Nav.Link>
+			<Nav.Link as={Link} to="/">Home</Nav.Link>
+			<Nav.Link as={Link} to="/calendar">Calendar</Nav.Link>
 		  </Nav>
 		</Navbar.Collapse>
 		</Container>
