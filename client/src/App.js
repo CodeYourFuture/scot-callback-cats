b221d83378d4ecd37427305ff28a69c426618742
aalassv <a.alas.sv@gmail.com>
2022-02-12 12:38:23 +0000

    Fixing navbar size

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,22 @@ import NavbarComponent from "./Component/NavbarComponent";
 import { Container } from "react-bootstrap";
 
 const App = () => (
-	<Container>
+	<>
 		<NavbarComponent />
-            <Switch>
-                <Route path="/" exact>
-                    <Home />
-                </Route>
-                <Route path="/login">
-                    <Login />
-                </Route>
-                <Route path="/calendar">
-                    <Calendar />
-                </Route>
-            </Switch>
-        </Container>
+		<Container>
+			<Switch>
+				<Route path="/" exact>
+					<Home />
+				</Route>
+				<Route path="/login">
+					<Login />
+				</Route>
+				<Route path="/calendar">
+					<Calendar />
+				</Route>
+			</Switch>
+		</Container>
+	</>
 );
 
 export default App;
